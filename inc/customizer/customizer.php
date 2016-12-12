0d7356edf169a8b009741f9c7400b92fe1f30318
Lenovo-PC\thomasw <kontakt@themecoder.de>
2016-12-12 19:39:00 +0100

    Changed demo links in Customizer and Theme Info

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -91,7 +91,7 @@ add_action( 'customize_preview_init', 'napoli_customize_preview_js' );
  * Embed CSS styles for the theme options in the Customizer
  */
 function napoli_customize_preview_css() {
-	wp_enqueue_style( 'napoli-customizer-css', get_template_directory_uri() . '/css/customizer.css', array(), '20160915' );
+	wp_enqueue_style( 'napoli-customizer-css', get_template_directory_uri() . '/css/customizer.css', array(), '20161214' );
 }
 add_action( 'customize_controls_print_styles', 'napoli_customize_preview_css' );
 
@@ -114,7 +114,7 @@ function napoli_customize_theme_links() {
 			</p>
 
 			<p>
-				<a href="http://preview.themezee.com/napoli/?utm_source=theme-info&utm_medium=textlink&utm_campaign=napoli&utm_content=demo" target="_blank">
+				<a href="http://preview.themezee.com/?demo=napoli&utm_source=theme-info&utm_medium=textlink&utm_campaign=napoli&utm_content=demo" target="_blank">
 					<?php esc_html_e( 'Theme Demo', 'napoli' ); ?>
 				</a>
 			</p>
