df92af5039880824188ec000d0d67be314f322b2
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-04-10 09:14:52 +0200

    fixed horizontal scrolling issue when elements are hidden on mobile devices

--- inc/extras.php
+++ inc/extras.php
@@ -103,7 +103,7 @@ function napoli_hide_elements() {
 
 	// Create CSS.
 	$classes = implode( ', ', $elements );
-	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); }';
+	$custom_css = $classes . ' { position: absolute; clip: rect(1px, 1px, 1px, 1px); width: 1px; height: 1px; overflow: hidden; }';
 
 	// Add Custom CSS.
 	wp_add_inline_style( 'napoli-stylesheet', $custom_css );
