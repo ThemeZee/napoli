98dad28bdb9b0c9bf8bcfa7418170b73402daeb2
Thomas W <kontakt@themecoder.de>
2017-08-08 11:01:49 +0200

    added new setting to deactivate Magazine widgets on blog index

--- inc/customizer/sections/customizer-blog.php
+++ inc/customizer/sections/customizer-blog.php
@@ -52,5 +52,31 @@ function napoli_customize_register_blog_settings( $wp_customize ) {
 		'type'     => 'textarea',
 		'priority' => 20,
 	) );
+
+	// Add Magazine Widgets Headline.
+	$wp_customize->add_control( new Napoli_Customize_Header_Control(
+		$wp_customize, 'napoli_theme_options[blog_magazine_widgets_title]', array(
+			'label'    => esc_html__( 'Magazine Widgets', 'napoli' ),
+			'section'  => 'napoli_section_blog',
+			'settings' => array(),
+			'priority' => 30,
+		)
+	) );
+
+	// Add Setting and Control for Magazine widgets.
+	$wp_customize->add_setting( 'napoli_theme_options[blog_magazine_widgets]', array(
+		'default'           => true,
+		'type'              => 'option',
+		'transport'         => 'refresh',
+		'sanitize_callback' => 'napoli_sanitize_checkbox',
+	) );
+
+	$wp_customize->add_control( 'napoli_theme_options[blog_magazine_widgets]', array(
+		'label'    => esc_html__( 'Display Magazine widgets on blog index', 'napoli' ),
+		'section'  => 'napoli_section_blog',
+		'settings' => 'napoli_theme_options[blog_magazine_widgets]',
+		'type'     => 'checkbox',
+		'priority' => 40,
+	) );
 }
 add_action( 'customize_register', 'napoli_customize_register_blog_settings' );
