151482b230ddb0eebfb1735536ad429dafc9fd18
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-08-18 17:59:05 +0200

    minified navigation script

--- functions.php
+++ functions.php
@@ -180,9 +180,9 @@ function napoli_scripts() {
 	wp_enqueue_script( 'html5shiv', get_template_directory_uri() . '/assets/js/html5shiv.min.js', array(), '3.7.3' );
 	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
-	// Register and enqueue navigation.js.
+	// Register and enqueue navigation.min.js.
 	if ( has_nav_menu( 'primary' ) || has_nav_menu( 'secondary' ) || has_nav_menu( 'social' ) ) {
-		wp_enqueue_script( 'napoli-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array( 'jquery' ), '20191114', true );
+		wp_enqueue_script( 'napoli-navigation', get_theme_file_uri( '/assets/js/navigation.min.js' ), array( 'jquery' ), '20200818', true );
 		$napoli_l10n = array(
 			'expand'   => esc_html__( 'Expand child menu', 'napoli' ),
 			'collapse' => esc_html__( 'Collapse child menu', 'napoli' ),
