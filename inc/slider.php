6fbddfd137d2dd1cf31fd3e176c864f37da38909
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-04 11:45:00 +0200

    updated flexslider css

--- inc/slider.php
+++ inc/slider.php
@@ -27,11 +27,7 @@ function napoli_slider_scripts() {
 		// Register and enqueue slider setup.
 		wp_enqueue_script( 'napoli-slider', get_template_directory_uri() . '/assets/js/slider.js', array( 'jquery-flexslider' ), '20170421' );
 
-		// Register and enqueue slider CSS.
-		wp_enqueue_style( 'napoli-slider', get_template_directory_uri() . '/assets/css/flexslider.css', array(), '20210303' );
-
 	endif;
-
 }
 add_action( 'wp_enqueue_scripts', 'napoli_slider_scripts' );
 
