2b79dc8a5bd7668ef7aa233ed1fa77af239da5fb
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-07-19 14:33:38 +0200

    removed Magazine Widget Customizer control

--- inc/magazine.php
+++ inc/magazine.php
@@ -35,12 +35,7 @@ function napoli_magazine_widgets() {
 
 		</div><!-- #magazine-homepage-widgets -->
 
-	<?php
-	elseif ( is_customize_preview() ) :
-
-		// Display Magazine Widget Placeholder in Customizer.
-		napoli_customize_magazine_placeholder();
-
+		<?php
 	elseif ( is_page_template( 'template-magazine.php' ) && current_user_can( 'edit_theme_options' ) ) :
 
 		echo '<p class="empty-widget-area">';
