656842108ee68bc8938dcc3d92d392df78ec2340
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-07-12 10:06:36 +0200

    added theme support for custom spacing and line heights

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -13,6 +13,12 @@
  */
 function napoli_gutenberg_support() {
 
+	// Add theme support for dimension controls.
+	add_theme_support( 'custom-spacing' );
+
+	// Add theme support for custom line heights.
+	add_theme_support( 'custom-line-height' );
+
 	// Define block color palette.
 	$color_palette = apply_filters( 'napoli_color_palette', array(
 		'primary_color'    => '#ee4455',
